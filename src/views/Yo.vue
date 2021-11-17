89ea508ad391691fb22d131c7d030a81f0d2c9e4
jdavid2019 <joseph2019d@gmail.com>
2021-11-17 15:50:01 -0500

    New changes

---
+++ src/views/Yo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="yo">
+    <h2 class="jeje">Muy buenas tardes</h2>
+
+    <p class="mi_texto">
+      Quien les habla es Luis Ángel Mendoza Chate, tengo 21 años. Me considero
+      una persona calmada con muchas ganas de aprender todo lo que tenga que ver
+      con la programación, sin embargo, a día de hoy estoy intentando
+      profundizar y especializarme en las tecnologías: Laravel, Vue.js, React.js
+      y Django... En mis pasatiempos suelo también ejercer algo de Blockchain
+      con Metamask, las Dapps, su lenguaje Solidity para Ethereum, y Ganache,
+      otra de las aficiones que tengo en relación al desarrollo de Software se
+      enfoca más en animaciones y diseño web. Lejos de esto soy amante de los
+      videojuegos, la música, el wrestling y la realización de cómics. Cuento
+      con una cuenta en Youtube en la cual presente diversos vídeos de los
+      muchos sistemas que he podido realizar a lo largo de mi vida universitaria
+      como de otros temas fuera de su contexto, por otra parte me mantengo
+      interesado por los artículos de investigación que se desarrollan muchos
+      Ing. de Sistemas como yo sea desde cualquier fuente o en ResearchGate, la
+      cual es el sitio donde también comparto parte de mis trabajos.
+    </p>
+  </div>
+</template>
+
+
+<style scoped>
+.jeje {
+  text-decoration: underline;
+}
+
+@media (min-width: 600px) {
+  .mi_texto {
+    padding: 0px 20px 0px 20px;
+    text-align: justify;
+  }
+}
+</style>
